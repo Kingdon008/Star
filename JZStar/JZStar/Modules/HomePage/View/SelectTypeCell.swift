45fad8308db7e702f45780e7cb6b8b3a618d0a92
dujie <dujie@sansi.com>
2020-06-03 16:55:29 +0800

    home

---
+++ JZStar/JZStar/Modules/HomePage/View/SelectTypeCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelectTypeCell.swift
+//  JZStar
+//
+//  Created by don on 2020/6/3.
+//  Copyright © 2020 don. All rights reserved.
+//
+
+import UIKit
+
+class SelectTypeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
